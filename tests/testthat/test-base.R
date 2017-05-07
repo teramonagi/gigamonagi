d3293ca55b299b2c1b1be9eeaa7befad3c72bc67
Nagi Teramo <teramonagi@gmail.com>
2017-05-07 14:50:11 +0900

    Update

--- tests/testthat/test-base.R
+++ tests/testthat/test-base.R
@@ -3,10 +3,12 @@ context("base.R")
 test_that("is_numeric() for vector", {
   x <- c(1, 2, NA, "AAA", "123")
   expect_equal(is_numeric(x), c(TRUE, TRUE, FALSE, FALSE, TRUE))
+  expect_equal(is_numeric(x, na=TRUE),  c(TRUE, TRUE, TRUE, FALSE, TRUE))
+  expect_equal(is_numeric(x, na=FALSE), c(TRUE, TRUE, FALSE, FALSE, TRUE))
 })
 
 test_that("is_numeric() for data.frame", {
   df <- dplyr::data_frame(a=c(NA, "A", "", "123", "432"))
   expect_equal(dplyr::filter(df, is_numeric(a)), dplyr::tibble(a=c("123", "432")))
-  expect_equal(dplyr::filter(df, is_numeric(a, na="123")), dplyr::tibble(a="432"))
+  expect_equal(dplyr::filter(df, is_numeric(a, na=TRUE)), dplyr::tibble(a=c(NA, "123", "432")))
 })
